b5f5966044a4df6074c657d995e67d33e692fce6
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-09-24 11:33:22 -0400

    Problem 12 solution added.

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -24,9 +24,9 @@ namespace DatabaseFirstLINQ
             //ProblemSeven();
             //ProblemEight();
             //ProblemNine();
-            ProblemTen();
+            //ProblemTen();
             //ProblemEleven();
-            //ProblemTwelve();
+            ProblemTwelve();
             //ProblemThirteen();
             //ProblemFourteen();
             //ProblemFifteen();
@@ -191,6 +191,14 @@ namespace DatabaseFirstLINQ
         private void ProblemTwelve()
         {
             // Create a new Product object and add that product to the Products table using LINQ.
+            Product newProduct = new Product()
+            {
+                Name = "Guitar",
+                Description = "Acoustic cutaway",
+                Price = 350
+            };
+            _context.Products.Add(newProduct);
+            _context.SaveChanges();
 
         }
 
